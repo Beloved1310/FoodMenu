05ad2f00bef00b0d75411fe5778e9a5eacfce87a
Adefisayo1310 <adefisayo@jamitfm.com>
2021-12-01 17:10:16 +0100

    docs: enable prettier to lint auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,19 +1,21 @@
 /* eslint consistent-return: "off" */
 
-const jwt = require('jsonwebtoken')
-const { JWT } = require('../config')
+const jwt = require('jsonwebtoken');
+const { JWT } = require('../config');
 
 module.exports = (req, res, next) => {
-  const token = req.header('x-auth-token')
+  const token = req.header('x-auth-token');
   if (!token) {
-    return res.status(401).send({ message: 'Access denied. No token provided' })
+    return res
+      .status(401)
+      .send({ message: 'Access denied. No token provided' });
   }
 
   try {
-    const decoded = jwt.verify(token, JWT)
-    req.user = decoded
-    next()
+    const decoded = jwt.verify(token, JWT);
+    req.user = decoded;
+    next();
   } catch (ex) {
-    res.status(400).send({ message: 'Invalid token.' })
+    res.status(400).send({ message: 'Invalid token.' });
   }
-}
+};
