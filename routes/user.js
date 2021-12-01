6fbbf961e2fb26b34650f4d5104ff58a1773b3aa
Adefisayo1310 <adefisayo@jamitfm.com>
2021-12-01 17:22:43 +0100

    rewrite user routes

--- routes/user.js
+++ routes/user.js
@@ -1,14 +1,13 @@
-const express = require('express')
-const asyncMiddleware = require('../middleware/async')
+const express = require('express');
+const asyncMiddleware = require('../middleware/async');
 
-const router = express.Router()
+const router = express.Router();
 
-const signUp = require('../controller/user/signUp')
-const login = require('../controller/user/login')
+const signUp = require('../controller/user/signUp');
+const login = require('../controller/user/login');
 
-router.post('/signup', asyncMiddleware(signUp))
+router.post('/signup', asyncMiddleware(signUp));
 
-router.post('/login', asyncMiddleware(login))
+router.post('/login', asyncMiddleware(login));
 
 module.exports = router;
-
