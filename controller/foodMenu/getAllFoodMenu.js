d94de3090a5bcd4fb1dc14695d9a8106b672e868
Adefisayo1310 <adefisayo@jamitfm.com>
2021-12-01 16:56:53 +0100

    feat: rewrite route to filter published menu and location

--- controller/foodMenu/getAllFoodMenu.js
+++
@@ -1,6 +0,0 @@
-const FoodMenu = require('../../models/foodMenu');
-
-module.exports = async (req, res) => {
-  const data = await FoodMenu.find({ ispublished: 'true' });
-  return res.send({ message: 'Food menu', data });
-};
