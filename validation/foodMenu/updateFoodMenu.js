e0b20095830cb811a7fc6d57aa037ef6f236f435
Adefisayo1310 <adefisayo@jamitfm.com>
2021-12-01 17:42:46 +0100

    feat: rewrite updateFoodMenu validation using joi

--- validation/foodMenu/updateFoodMenu.js
+++ validation/foodMenu/updateFoodMenu.js
@@ -5,9 +5,9 @@ module.exports = function validate(input) {
     name: Joi.string(),
     quantity: Joi.number().positive(),
     price: Joi.number().positive(),
-    location:Joi.string(),
-    category: Joi.string().valid('breakfast', 'lunch', 'bunch', 'dinner'),
-    ispublished: Joi.boolean()
+    location: Joi.string(),
+    category: Joi.string(),
+    ispublished: Joi.boolean(),
   });
   return schema.validate(input);
 };
