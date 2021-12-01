dc36f696c01eb70f728263d65aadf62e6e29662e
Adefisayo1310 <adefisayo@jamitfm.com>
2021-12-01 17:02:53 +0100

    feat: rewrite login controller

--- controller/user/login.js
+++ controller/user/login.js
@@ -1,23 +1,23 @@
-const bcrypt = require('bcrypt')
+const bcrypt = require('bcrypt');
 
-const User = require('../../models/user')
-const loginValidate = require('../../validation/user/loginValidate')
+const User = require('../../models/user');
+const loginValidate = require('../../validation/user/loginValidate');
 
 module.exports = async (req, res) => {
-  const { value, error } = loginValidate(req.body)
-  if (error) return res.status(400).send({ error: error.details[0].message })
-  const { email, password } = value
+  const { value, error } = loginValidate(req.body);
+  if (error) return res.status(400).send({ error: error.details[0].message });
+  const { email, password } = value;
 
-  const user = await User.findOne({ email })
+  const user = await User.findOne({ email });
   if (!user)
-    return res.status(400).send({ error: 'username or password not found' })
+    return res.status(400).send({ error: 'username or password not found' });
 
-  const validPassword = await bcrypt.compare(password, user.password)
+  const validPassword = await bcrypt.compare(password, user.password);
   if (!validPassword)
-    return res.status(400).send({ error: 'username or password not found ' })
+    return res.status(400).send({ error: 'username or password not found ' });
 
-  const token = user.generateAuthToken()
-  res.header('x-auth-token', token)
-  const data = { email, token }
-  return res.send({ message: 'Login Successful', data })
-}
+  const token = user.generateAuthToken();
+  res.header('x-auth-token', token);
+  const data = { email, token };
+  return res.send({ message: 'Login Successful', data });
+};
